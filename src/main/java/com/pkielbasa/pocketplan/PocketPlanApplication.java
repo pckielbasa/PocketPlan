78bae68f267ba2054ae1c744bb89fc758127649e
pckielbasa <pckielbasa@gmail.com>
2025-06-07 19:43:24 +0200

    Initial commit

---
+++ src/main/java/com/pkielbasa/pocketplan/PocketPlanApplication.java
@@ -0,0 +1,13 @@
+package com.pkielbasa.pocketplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocketPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pkielbasa/pocketplan/PocketPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkielbasa.pocketplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
