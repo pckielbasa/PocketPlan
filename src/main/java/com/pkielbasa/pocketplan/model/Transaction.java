53da83e9403049f48cb5016b76cd9199b7449ad1
pckielbasa <pckielbasa@gmail.com>
2025-06-15 15:11:09 +0200

    DB and LB configurations

---
+++ src/main/java/com/pkielbasa/pocketplan/model/Transaction.java
@@ -0,0 +1,32 @@
+package com.pkielbasa.pocketplan.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "Transaction")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "budget_id", nullable = false)
+    private Budget budget;
+    private String name;
+    private String description;
+    private String type;
+
+
+
+}
+
+
---
+++ src/main/java/com/pkielbasa/pocketplan/model/User.java
@@ -0,0 +1,32 @@
+package com.pkielbasa.pocketplan.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "User")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 255)
+    private String email;
+
+    @Column(name = "first_name", nullable = false, length = 255)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 255)
+    private String lastName;
+}
