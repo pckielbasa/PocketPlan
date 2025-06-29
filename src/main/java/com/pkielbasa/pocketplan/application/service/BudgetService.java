1350e0187af73ca57abb16e44a5e2d1279941eb9
pckielbasa <pckielbasa@gmail.com>
2025-06-29 15:58:16 +0200

    Adding new dependencies between user and budget, removed unique from initial budget

--- src/main/java/com/pkielbasa/pocketplan/application/service/BudgetService.java
+++ src/main/java/com/pkielbasa/pocketplan/application/service/BudgetService.java
@@ -1,7 +1,10 @@
 package com.pkielbasa.pocketplan.application.service;
 
 import com.pkielbasa.pocketplan.domain.model.Budget;
+import com.pkielbasa.pocketplan.domain.model.User;
 import com.pkielbasa.pocketplan.domain.repository.BudgetRepository;
+import com.pkielbasa.pocketplan.domain.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +13,15 @@ import org.springframework.stereotype.Service;
 public class BudgetService {
 
     private final BudgetRepository budgetRepository;
+    private final UserRepository userRepository;
 
-    public void createBudget(Budget budget) {
+    public void createBudget(Budget budget, long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("User not found"));
+
+        user.getBudgets().add(budget);
+        budget.getUsers().add(user);
+
         budgetRepository.createBudget(budget);
     }
 }
