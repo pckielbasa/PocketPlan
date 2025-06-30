8df46a624c23f3cf29a52a183ed56c52cf98b93b
pckielbasa <pckielbasa@gmail.com>
2025-06-30 19:25:36 +0200

    Refactor

--- src/main/java/com/pkielbasa/pocketplan/application/util/EntityFetcherService.java
+++ src/main/java/com/pkielbasa/pocketplan/application/util/EntityFetcherService.java
@@ -1,10 +1,12 @@
 package com.pkielbasa.pocketplan.application.util;
 
-import com.pkielbasa.pocketplan.exception.ResourceNotFoundException;
 import com.pkielbasa.pocketplan.domain.model.Budget;
 import com.pkielbasa.pocketplan.domain.model.Transaction;
+import com.pkielbasa.pocketplan.domain.model.User;
 import com.pkielbasa.pocketplan.domain.repository.BudgetRepository;
 import com.pkielbasa.pocketplan.domain.repository.TransactionRepository;
+import com.pkielbasa.pocketplan.domain.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +16,17 @@ public class EntityFetcherService {
 
     private final TransactionRepository transactionRepository;
     private final BudgetRepository budgetRepository;
+    private final UserRepository userRepository;
 
-    public Transaction fetchTransactionOrThrow(Long id) {
-        return transactionRepository.getTransactionById(id).orElseThrow(()-> new ResourceNotFoundException("Transaction by id: " + id + " not found"));
+    public Transaction fetchTransactionOrThrow(Long transactionId) {
+        return transactionRepository.getTransactionById(transactionId).orElseThrow(()-> new EntityNotFoundException("Transaction by id: " + transactionId + " not found"));
     }
 
-    public Budget fetchBudgetOrThrow(Long id) {
-        return budgetRepository.getBudgetById(id).orElseThrow(()-> new ResourceNotFoundException("Budget by id: " + id + " not found"));
+    public Budget fetchBudgetOrThrow(Long budgetId) {
+        return budgetRepository.getBudgetById(budgetId).orElseThrow(()-> new EntityNotFoundException("Budget by id: " + budgetId + " not found"));
+    }
+
+    public User fetchUserOrThrow(Long userId) {
+        return userRepository.getUserById(userId).orElseThrow(()-> new EntityNotFoundException("User by id: " + userId + " not found"));
     }
 }
